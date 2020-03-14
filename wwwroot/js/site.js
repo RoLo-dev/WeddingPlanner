ab7f5348e354116263bfde96085e7e76e304bca9
Rolando Lopantzi <rlopantzi@gmail.com>
2020-03-14 14:22:27 -0700

    update files

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,38 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+const open = document.getElementById('open');
+const close = document.getElementById('close');
+const nav = document.getElementById('mobileNav');
+
+open.addEventListener('click', reveal);
+close.addEventListener('click', exit);
+
+function reveal(){
+    nav.classList.add('show');
+    console.log('clicked');
+}
+function exit(){
+    nav.classList.remove('show');
+}
+
+$(document).ready(function(){
+    // Add smooth scrolling to all links
+    $(".show").on('click', function(event) {
+
+        if (this.hash !== "b") {
+        // Prevent default anchor click behavior
+        event.preventDefault();
+
+        // Store hash
+        var hash = this.hash;
+
+        $('html, body').animate({
+            scrollTop: $(hash).offset().top
+        }, 600, function(){
+
+            window.location.hash = hash;
+        });
+        } // End if
+    });
+});
